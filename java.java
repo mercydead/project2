eea05f22b956a8565d4655071ff81d47751f1c37
mercydead <mercypilldeaddeal@gmail.com>
2025-12-05 12:18:33 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,1 +1,6 @@
-system.out.println("java")
+public class p2{
+  public static void main(String args[]){
+    System.out.println("Java file execuiton");
+    System.out.println("Hello world");
+  }
+}
